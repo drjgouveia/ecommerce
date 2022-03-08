47d8a5759facd3d659ca58e572c0f401619fc192
drjgouveia <diogo.gouveia2@gmail.com>
2022-03-08 20:59:39 +0100

    Have now a temporary homepage, that is being edited to be used in conjunction with the CRUDs API.

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -3,8 +3,18 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.urls import reverse
+from sells.models import Sell
+from prods.models import Product
 
 
 @login_required(login_url="auth/login")
 def homepage(request):
-    return render(request, 'homepage.html')
+    """
+    Loads data for the homepage
+
+    :param request: user request
+    :return: returns the page with the rendered information
+    """
+    prods = Product.objects.all()
+    sells = Sell.objects.all()
+    return render(request, 'homepage.html', {"prods": prods, "sells": sells})
