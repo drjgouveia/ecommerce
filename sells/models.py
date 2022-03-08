ab4861828d070d129d1e90e14a07978dfcf67dc7
drjgouveia <diogo.gouveia2@gmail.com>
2022-03-08 21:29:18 +0100

    Quantity is now Decimal

--- sells/models.py
+++ sells/models.py
@@ -13,5 +13,5 @@ class SellDetails(models.Model):
     # id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     id = models.AutoField(primary_key=True, editable=False)
     product = models.ForeignKey(to="prods.Product", on_delete=models.SET_NULL, null=True)
-    quantity = models.IntegerField()
     total = models.DecimalField(max_digits=7, decimal_places=2)
+    quantity = models.DecimalField(max_digits=7, decimal_places=2)
